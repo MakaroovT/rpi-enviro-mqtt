12e019be83b9e53c47c97432ae18facb8bd5a353
MakaroovT <g-cyou@nissin-technis.jp>
2021-12-01 14:51:50 +0900

    master

--- mqtt-get-20211201.py
+++ mqtt-get-20211201.py
@@ -3,14 +3,11 @@ import paho.mqtt.client as mqttClient
 import time
 import datetime
 import os
-#import logging
 
-# 要连接的服务器与端口号
-broker_address = "10.10.10.15"  # Broker address
-port = 1883  # Broker port
-# 要订阅的主题
+
+broker_address = "10.10.10.15"  
+port = 1883  
 subscribe = "nissin/env-data"
-# 打印还是保存,打印哪种数据
 save2file = True
 jsonorhex = True
 
@@ -18,8 +15,8 @@ jsonorhex = True
 def on_connect(client, userdata, flags, rc):
     if rc == 0:
         print("Connected to broker")
-        global Connected  # Use global variable
-        Connected = True  # Signal connection
+        global Connected  
+        Connected = True  
 
     else:
         print("Connection failed")
@@ -28,10 +25,10 @@ def on_connect(client, userdata, flags, rc):
 def on_message(client, userdata, message):
     if save2file:
         try:
-            today = datetime.date.today()  # Get OS time
-            year = today.year  # Get year to built folder
-            folder_path = "C:/" + str(year) + "/"  # Set folder path
-            if not os.path.exists(folder_path):  # Determine whether the folder exists, if it does not exist create it
+            today = datetime.date.today()  
+            year = today.year  
+            folder_path = "C:/" + str(year) + "/"  
+            if not os.path.exists(folder_path):  
                 os.makedirs(folder_path)
             with open(folder_path + format(today) + '.txt', 'a', encoding='UTF-8') as f:
                 if jsonorhex:
@@ -39,7 +36,7 @@ def on_message(client, userdata, message):
                                 encoding='utf-8') + "\n")
                 else:
                     f.write(message.payload.hex().upper() + "\n")
-                f.close()  # close data
+                f.close()  
         except Exception as e:
             logging.error('Caught exception error: ' + str(e))
     else:
@@ -51,11 +48,11 @@ def on_message(client, userdata, message):
     
     time.sleep(5)
 
-Connected = False  # global variable for the state of the connection
 
-client = mqttClient.Client("clientid1")  # create new instance
-client.on_connect = on_connect  # attach function to callback
-client.on_message = on_message  # attach function to callback
-client.connect(broker_address, port, 5)  # connect
-client.subscribe(subscribe)  # subscribe
-client.loop_forever()  # then keep listening forever
+Connected = False  
+client = mqttClient.Client("clientid1")  
+client.on_connect = on_connect 
+client.on_message = on_message  
+client.connect(broker_address, port, 5) 
+client.subscribe(subscribe)  
+client.loop_forever()  
